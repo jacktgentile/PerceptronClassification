7f14c4e5bf89f02c39cc1ce538df435017d40349
aukey2 <aukey2@illinois.edu>
2019-04-20 00:37:27 -0500

    test_relu pass

--- mp4/part 2/neural_network.py
+++ mp4/part 2/neural_network.py
@@ -67,17 +67,104 @@ def four_nn():
     Hint: Utilize numpy as much as possible for max efficiency.
         This is a great time to review on your linear algebra as well.
 """
+# TODO: replace naive python loops with numpy multiplication
 def affine_forward(A, W, b):
+    z_rows = A.shape[0]
+    z_cols = W.shape[1]
+    Z = np.zeros((z_rows, z_cols))
+    d = W.shape[0]
+
+    for i in range(z_rows):
+        for j in range(z_cols):
+            z_ij = 0
+            for k in range(d):
+                z_ij += A[i][k] * W[k][j] + b[j]
+            Z[i][j] = z_ij
+
+    cache = [A, W]
     return Z, cache
 
+# TODO: replace naive python loops with numpy multiplication
 def affine_backward(dZ, cache):
-    return dA, dW, dB
+    A = cache[0]
+    W = cache[1]
+
+    dA = np.zeros(A.shape)
+    dW = np.zeros(W.shape)
+    db = np.zeros((W.shape[1]))
+
+    n = A.shape[0]
+    d_prime = W.shape[1]
+
+    # Computing dA
+    for i in range(dA.shape[0]):
+        for k in range(dA.shape[1]):
+            dA_ik = 0
+            for j in range(d_prime):
+                dA_ik += dZ[i][j] * W[k][j]
+            dA[i][k] = dA_ik
+
+    # Computing dW
+    for k in range(dW.shape[0]):
+        for j in range(dW.shape[1]):
+            dW_kj = 0
+            for i in range(n):
+                dW_kj += A[i][k] * dZ[i][j]
+            dW[k][j] = dW_kj
+
+    # Computing db
+    for j in range(db.shape[0]):
+        db_j = 0
+        for i in range(n):
+            db_j += dZ[i][j]
+        db[j] = db_j
+
+    return dA, dW, db
 
 def relu_forward(Z):
+    A = np.maximum(Z, 0)
+
+    cache = [A]
     return A, cache
 
 def relu_backward(dA, cache):
-    return dA
+    Z = cache[0]
+    dZ = np.where(Z <= 0, 0, dA)
 
+    return dZ
+
+# TODO: replace naive python loops with numpy multiplication
 def cross_entropy(F, y):
+    n = F.shape[0]
+    C = F.shape[1]
+
+    # Computing loss
+    loss = 0
+    for i in range(n):
+        loss += F[i][y[i]]
+
+        loss_sum = 0
+        for k in range(C):
+            loss_sum += np.exp(F[i][k])
+        loss -= np.log(loss_sum)
+
+    loss *= -(1/n)
+
+    # Computing dF
+    dF = np.zeros(F.shape)
+    for i in range(F.shape[0]):
+        for j in range(F.shape[1]):
+            dF_ij = 0
+
+            if j == y[i]:
+                dF_ij += 1
+
+            dF_sum = 0
+            for k in range(C):
+                dF_sum += np.exp(F[i][k])
+
+            dF_ij -= np.exp(F[i][j]) / dF_sum
+            dF_ij *= -(1/n)
+            dF[i][j] = dF_ij
+
     return loss, dF
