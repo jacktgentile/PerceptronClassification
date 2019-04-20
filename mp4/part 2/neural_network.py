b12c66a383b5a181682e9aae1d3388efc0276942
aukey2 <aukey2@illinois.edu>
2019-04-20 03:26:21 -0500

    Initial attempt at four_nn function

--- mp4/part 2/neural_network.py
+++ mp4/part 2/neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+eta = 0.1
 """
     Minigratch Gradient Descent Function to train model
     1. Format the data
@@ -55,8 +56,29 @@ def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
     Up to you on how to implement this, won't be unit tested
     Should call helper functions below
 """
-def four_nn():
-    pass
+def four_nn(X, Ws, bs, y, test):
+    Z1, acache1 = affine_forward(X, Ws[0], bs[0])
+    A1, rcache1 = relu_forward(Z1)
+    Z2, acache2 = affine_forward(A1, Ws[1], bs[1])
+    A2, rcache2 = relu_forward(Z2)
+    Z3, acache3 = affine_forward(A2, Ws[2], bs[2])
+    A3, rcache3 = relu_forward(Z3)
+    F, acache4 = affine_forward(A2, Ws[3], bs[3])
+
+    if test == true:
+        pass
+
+    loss, dF = cross_entropy(F, y)
+    dA3, dW4, db4 = affine_backward(dF, acache4)
+    dZ3 = relu_backward(dA3, rcache3)
+    dA2, dW3, db3 = affine_backward(dZ3, acache3)
+    dZ2 = relu_backward(dA2, rcache2)
+    dA1, dW2, db2 = affine_backward(dZ2, acache2)
+    dZ1 = relu_backward(dA1, rcache1)
+    dX, dW1, db1 = affine_backward(dZ1, acache1)
+    Ws[0] -= eta * dW1
+
+    return loss
 
 """
     Next five functions will be used in four_nn() as helper functions.
