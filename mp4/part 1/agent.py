251bc7ed7bc18bbfafa72a1ea8e192f8fdfe586d
aukey2 <aukey2@illinois.edu>
2019-04-22 06:40:00 -0500

    Getting one food

--- mp4/part 1/agent.py
+++ mp4/part 1/agent.py
@@ -163,7 +163,7 @@ class Agent:
         (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the 480x480 board)
 
         '''
-
+        # print(state[0] // SEGMENT_SIZE, state[1] // SEGMENT_SIZE)
         curr_state = self.convert_state(state)
 
         if self._train:
@@ -172,9 +172,11 @@ class Agent:
                 # Updating our Q value
                 alpha = self.get_alpha()
                 R_s = self.R(self.s, points, dead)
-                max_curr_q, max_curr_a = self.best_move(curr_state)
+                max_curr_a, max_curr_q  = self.best_move(curr_state)
                 new_q = (1 - alpha) * self.fQ(self.s, self.a) + alpha * (R_s + self.gamma * max_curr_q)
                 self.update_Q(self.s, self.a, new_q)
+
+                # print("updating at coor", state[0] // SEGMENT_SIZE, state[1] // SEGMENT_SIZE, "with q: ", new_q)
 
                 if not dead:
                     self.increment_N(self.s, self.a)
